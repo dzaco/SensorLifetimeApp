866bdcf8992a5b5fe90ebbc228244bca6d9c0d39
Jacek Giedronowicz <jgiedronowicz@kpmg.pl>
2021-04-02 13:21:30 +0200

    Add project files.

---
+++ SensorLifetimeApp/Commons/ParamSetup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLifetimeApp.Commons
+{
+    class ParamSetup // Singleton
+    {
+
+        public float MinimalCoverage { get; }
+        public int PoiCount { get; }
+        public int SensorCount { get; }
+        public int BatteryCapacity { get; }
+        public int AreaWidth { get; }
+
+        private ParamSetup() // Set default values
+        {
+            this.MinimalCoverage = 0.9f;
+            this.PoiCount = 36;
+            this.SensorCount = 5;
+            this.BatteryCapacity = 20;
+            this.AreaWidth = 100;
+        }
+        private ParamSetup(float minCov, int poi, int sensors, int battery, int area)
+        {
+            _instance = null; // reset parameters;
+            this.MinimalCoverage = minCov;
+            this.PoiCount = poi;
+            this.SensorCount = sensors;
+            this.BatteryCapacity = battery;
+            this.AreaWidth = area;
+        }
+
+        public ParamSetup(ParamSetupArray parameters) 
+            : this(parameters.MinimalCoverage, parameters.PoiCount, parameters.SensorCount, parameters.BatteryCapacity, parameters.AreaWidth)
+        { }
+
+        private static ParamSetup _instance;
+
+        public string Value {get;set;}
+        private static readonly object _lock = new object();
+
+        public static ParamSetup GetInstance(string value)
+        {
+            if(_instance == null)
+            {
+                lock(_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new ParamSetup();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+        public static ParamSetup GetInstance()
+        {
+            return ParamSetup.GetInstance("Singleton with program parameters");
+        }
+        public static ParamSetup GetInstance(ParamSetupArray parameters)
+        {
+            _instance = new ParamSetup(parameters);
+            return _instance;
+        }
+
+    }
+
+    class ParamSetupArray
+    {
+        public float MinimalCoverage { get; set; }
+        public int PoiCount { get; set; }
+        public int SensorCount { get; set; }
+        public int BatteryCapacity { get; set; }
+        public int AreaWidth { get; set; }
+    }
+}
---
+++ SensorLifetimeApp/Tests/TestsClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SensorLifetimeApp.Commons;
+
+namespace SensorLifetimeApp.Tests
+{
+    [TestFixture]
+    class TestsClass
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Console.WriteLine(
+              "{0}\n{1}\n\n{2}\n",
+              "If you see the same value, then singleton was reused (yay!)",
+              "If you see different values, then 2 singletons were created (booo!!)",
+              "RESULT:"
+          );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+        }
+        public static void TestSingleton(string value)
+        {
+            ParamSetup singleton = ParamSetup.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+    
+    }
+}
