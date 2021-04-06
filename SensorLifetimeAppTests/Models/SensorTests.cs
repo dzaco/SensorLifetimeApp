5311c012c7494da1e4c4597775987a8cc2a9cb08
Jacek Giedronowicz <jgiedronowicz@kpmg.pl>
2021-04-06 20:09:46 +0200

    Close XML reader/writer

--- SensorLifetimeAppTests/Models/SensorTests.cs
+++ SensorLifetimeAppTests/Models/SensorTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SensorLifetimeApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SensorLifetimeApp.Models.Tests
 {
@@ -16,11 +10,11 @@ namespace SensorLifetimeApp.Models.Tests
         {
             var path = @"C:\Users\Dzaco\Desktop\Private\project\SensorLifetimeApp\SensorLifetimeApp\Resources\SensorState.xml";
             var sensor = new Sensor(2, 10, 10, null);
-            //sensor.WriteToFile(path);
+            sensor.WriteToFile(path);
 
 
             var sensor2 = Sensor.ReadFromFile(path);
-            Assert.IsTrue(sensor == sensor2);
+            Assert.IsTrue(sensor.Equals(sensor2));
 
         }
 
