089bede582b4e3ebd0c2f820e479dbe386747269
Jacek Giedronowicz <jgiedronowicz@kpmg.pl>
2021-04-07 13:35:43 +0200

    no matter

---
+++ SensorLifetimeApp/Commons/Strings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLifetimeApp.Commons
+{
+    public class Strings
+    {
+        public class pl
+        {
+            public static string ShowPOI = "Wyświetl POI";
+        }
+
+        public class en
+        {
+            public static string ShowPOI = "Show POIs";
+        }
+
+    }
+}
