a447e113947a027804ae47e79180e345bfdd0aa1
Jacek Giedronowicz <jgiedronowicz@kpmg.pl>
2021-04-10 09:41:10 +0200

    Added FileException

---
+++ SensorLifetimeApp/Commons/Exceptions/ExceptionWithMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SensorLifetimeApp.Commons.Exceptions
+{
+    public class ExceptionWithMessage : Exception
+    {
+        public ExceptionWithMessage(string msg)
+        {
+            MessageBox.Show(msg, this.GetType().Name);
+        }
+    }
+}
---
+++ SensorLifetimeApp/Commons/Exceptions/FileException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLifetimeApp.Commons.Exceptions
+{
+    public class FileException : ExceptionWithMessage
+    {
+        public FileException(string msg) : base(msg) { }
+        public FileException() : base(Properties.Strings.FileNotFind) { }
+    }
+}
